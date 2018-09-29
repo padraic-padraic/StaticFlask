cba07bdd5832ab4b6283f83ad3b2e5ca9e415a45
Padraic Calpin <padraic.calpin93@gmail.com>
2018-09-29 12:05:13 +0100

    Move to my Flatpages fork

--- setup.sh
+++
@@ -1,9 +0,0 @@
-pyvenv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-git clone https://github.com/google/py-gfm
-cd py-gfm
-python3 setup.py build && python3 setup.py install
-cd ../StaticFlask
-python3 freeze.py
-echo 'Now edit your default apache/nginx conf to point to /var/www/Static'
